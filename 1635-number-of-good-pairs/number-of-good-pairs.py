a6dff8d3ed82796f33273d315301e52581e96a6d
shaik sofia parvez <shaiksofia6300@gmail.com>
2025-12-24 18:10:38 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (94.47%) - LeetSync

---
+++ 1635-number-of-good-pairs/number-of-good-pairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count = {}
+        good_pairs = 0
+
+        for num in nums:
+            if num in count:
+                good_pairs += count[num]
+                count[num] += 1
+            else:
+                count[num] = 1
+
+        return good_pairs
+        
